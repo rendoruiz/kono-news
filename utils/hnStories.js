7b6d3b0e325e01d25fda247b9ebe13a8f14e0ce8
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-07-01 13:48:30 -0600

    Update hnStories.js

--- utils/hnStories.js
+++ utils/hnStories.js
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { HN_ENDPOINT_PREFIX } from ".";
 
 // https://github.com/HackerNews/API
-const ENDPOINT_PREFIX = 'https://hacker-news.firebaseio.com/v0/';
+const ENDPOINT_PREFIX = HN_ENDPOINT_PREFIX;
 
 export const getStoryIdList = async (storyType) => {
-  const storyListEndpoint = ENDPOINT_PREFIX + storyType + 'stories.json';
+  const storyListEndpoint = ENDPOINT_PREFIX + `${storyType}stories.json`;
   const response = await axios.get(storyListEndpoint);
   return response.data;
 }
 
 export const getStoryData = async (storyId) => {
-  const storyDataEndpoint = ENDPOINT_PREFIX + 'item/' + storyId + '.json';
+  const storyDataEndpoint = ENDPOINT_PREFIX + `item/${storyId}.json`;
   const response = await axios.get(storyDataEndpoint);
   return response.data;
 }
