daf46aa538ec9823c7a01d8704fcb1f52b28a0a9
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-07-16 20:38:36 -0600

    progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
 import '../styles/reset.css'
 import '../styles/globals.css'
 
+const queryClient = new QueryClient();
+
 const MyApp = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
 )
 
 export default MyApp;
---
+++ pages/app/index.js
@@ -0,0 +1,389 @@
+import styled from "@emotion/styled";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useQueries } from "react-query";
+
+
+
+
+const StyledAppLayout = styled.div`
+  min-width: 300px;
+  min-height: 400px;
+  width: 100vw;
+  height: 100vh;
+  
+  background-color: orchid;
+`;
+const AppPage = ({ query, initialStoryListIds, initialStoryCommentsData, storyListPage, }) => {
+  const storyListIds = initialStoryListIds;
+  const storyCommentsData = initialStoryCommentsData;
+
+  console.log({query, initialStoryListIds, initialStoryCommentsData, storyListPage});
+
+  return (  
+    <StyledAppLayout>
+      {/* Navigation Panel */}
+      {/* 
+        Component States:
+        selectedStoryListMode
+        isExpanded, toggleNPExpansion
+      */}
+      <NavigationPanel />
+
+      {/* Story List Panel */}
+      {/* 
+        Component States:
+        selectedStoryItemData
+      */}
+      {/* 
+        Shared Component States:
+        selectedStoryListMode
+        toggleNPExpansion, toggleSCPExpansion 
+      */}
+      <StoryListPanel 
+        storyListMode={null} 
+        storyListIds={storyListIds} 
+      />
+
+      {/* Story Comments Panel */}
+      {/* 
+        Component States:
+        isExpanded, toggleSCPExpansion
+      */}
+      {/* 
+        Shared Component States:
+        selectedStoryItemData 
+      */}
+      <StoryCommentsPanel {...storyCommentsData} />
+    </StyledAppLayout>
+  );
+}
+
+
+//#region utils
+const STORIES_PER_PAGE = 30;
+
+const storyDataQueryParams = {
+  staleTime: 300000,
+  cacheTime: 900000,
+  keepPreviousData: true,
+}
+
+const STORYMODE = {
+  TOP: {
+    label: 'Home',
+    apiQuery: 'topstories',
+  },
+  NEW: {
+    label: 'Newest',
+    apiQuery: 'newstories',
+  },
+  BEST: {
+    label: 'Best',
+    apiQuery: 'beststories',
+  },
+  ASK: {
+    label: 'Ask',
+    apiQuery: 'askstories',
+  },
+  SHOW: {
+    label: 'Show',
+    apiQuery: 'showstories',
+  },
+  JOB:  {
+    label: 'Jobs',
+    apiQuery: 'jobstories',
+  },
+}
+
+const getParsedStoryListMode = (modeString) => {
+  if (!modeString) {
+    return STORYMODE.TOP;
+  }
+
+  const result = Object.keys(STORYMODE).filter((storyMode) => storyMode === modeString.toUpperCase()).pop();
+  return result ? STORYMODE[result] : STORYMODE.TOP;
+}
+
+const HN_API_ENDPOINT = 'https://hacker-news.firebaseio.com/v0/'
+const getStoryListIdsEndpoint = (storyListMode) => `${HN_API_ENDPOINT}${storyListMode}.json`;
+const getStoryDataEndpoint = (storyId) => `${HN_API_ENDPOINT}item/${storyId}.json`;
+
+const ALGOLIA_API_ENDPOINT = 'https://hn.algolia.com/api/v1/';
+const getStoryCommentDataEndpoint = (commentDataId) =>`${ALGOLIA_API_ENDPOINT}items/${commentDataId}`;
+
+
+const getStoryListIds = async (modeString) => {
+  const storyListMode = getParsedStoryListMode(modeString);
+  const endpoint = getStoryListIdsEndpoint(storyListMode.apiQuery);
+  try {
+    const response = await axios.get(endpoint);
+    return response.data;
+  } catch {
+    return { 
+      error: 'Failed to get story ids.' 
+    };
+  }
+}
+
+const getStoryData = async (storyId) => {
+  const endpoint = getStoryDataEndpoint(storyId);
+  try {
+    const response = await axios.get(endpoint);
+    return response.data;
+  } catch {
+    throw new Error('Failed to fetch story: ' + endpoint);
+  }
+}
+
+const getStoryCommentData = async (storyCommentId) => {
+  if (!storyCommentId) {
+    return null;
+  }
+
+  const storyEndpoint = getStoryDataEndpoint(storyCommentId);
+  const commentEndpoint = getStoryCommentDataEndpoint(storyCommentId);
+  try {
+    const response = await Promise.all(
+      [commentEndpoint, storyEndpoint].map((endpoint) => axios.get(endpoint))
+    );
+    return {
+      ...response[0].data,
+      post_count: response[1].data.descendants,
+    };
+  } catch {
+    return { 
+      error: 'Failed to get story comment data.' 
+    };
+  }
+}
+
+
+
+//#endregion
+
+export const getServerSideProps = async ({ query }) => {
+  const { 
+    mode: listMode, 
+    page: listPage, 
+    scid: storyCommentId 
+  } = query;
+
+  const storyListIds = await getStoryListIds(listMode);
+  const storyCommentsData = await getStoryCommentData(storyCommentId);
+
+  return {
+    props: {
+      query,
+      initialStoryListIds: storyListIds,
+      initialStoryCommentsData: storyCommentsData,
+      storyListPage: listPage ?? null,
+    }
+  }
+}
+
+
+//#region Navigation Panel
+const StyledNavigationPanel = styled.section``;
+
+const NavigationPanel = ({}) => {
+  return (
+    <StyledNavigationPanel>
+
+    </StyledNavigationPanel>
+  );
+}
+//#endregion
+
+
+//#region story list
+const StyledStoryListPanel = styled.section``;
+const StyledStoryListHeader = styled.div``;
+const StyledStoryList = styled.ol``;
+const StyledStoryItem = styled.li``;
+const StoryListPanel = ({ storyListMode, storyListIds }) => {
+  if (!storyListIds) {
+    return null;
+  }
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [storyListData, setStoryListData] = useState([]);
+  const currentStoryCount = STORIES_PER_PAGE * currentPage;
+  const isPageLimitReached = currentStoryCount >= storyListIds.length 
+    ? true 
+    : false;
+  const currentStoryIds = !isPageLimitReached 
+    ? storyListIds.slice(0, currentStoryCount) 
+    : storyListIds;
+
+  const storyListDataQuery = useQueries(
+    currentStoryIds.map((storyId) => {
+      return { 
+        queryKey: ['storydata', storyId], 
+        queryFn: () => getStoryData(storyId),
+        storyDataQueryParams,
+      }
+    }));
+  const isLoading = storyListDataQuery.some(query => query.isLoading);
+  const isError = storyListDataQuery.some(query => query.isError);
+  
+
+  
+
+  
+  return (
+    <StyledStoryListPanel>
+      {/* header w/ nav toggle */}
+      <StoryListHeader listMode={storyListMode} />
+
+      {isLoading && (
+        <p>Loading story list...</p>
+      )}
+
+      {isError && (
+        <p>Loading story list...</p>
+      )}
+
+      {/* story list */}
+      {!isLoading && !isError && (
+        <StoryList storyListData={storyListData} />
+      )}
+
+      {!isPageLimitReached && (
+        <button 
+          type='button'
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          load more
+        </button>
+      )}
+      
+      {/* story list propagation button */}
+    </StyledStoryListPanel>
+  );
+}
+
+const StoryListHeader = ({ listMode }) => {   // deconstruct props
+  return (
+    <StyledStoryListHeader>
+
+    </StyledStoryListHeader>
+  );
+}
+
+const StoryList = ({ storyListData }) => {
+  if (!storyListData) {
+    return null;
+  }
+
+  return (
+    <StyledStoryList>
+      {storyListData.map((storyItemData, index) => 
+        <Storyitem
+          key={storyItemData.id ?? index}
+          {...storyItemData}
+        >
+
+        </Storyitem>
+      )}
+    </StyledStoryList>
+  );
+}
+
+const Storyitem = ({ 
+  id,
+  title,
+  url,
+  by: author,
+  score: points,
+  descendants: post_count,
+}) => {
+  return (
+    <StyledStoryItem>
+      {title}
+    </StyledStoryItem>
+  );
+}
+//#endregion
+
+
+//#region storycomments
+const StyledStoryCommentsPanel = styled.section``;
+const StyledStoryCommentsHeader = styled.div``;
+const StyledStoryCommentsList = styled.ol``;
+const StyledStoryCommentsItem = styled.li``;
+const StoryCommentsPanel = ({ 
+  id,
+  title,
+  url,
+  author,
+  points,
+  post_count,
+  children,
+}) => {
+  return (
+    <StyledStoryCommentsPanel>
+      {/* header with: back button (close story), full screen, story urls */}
+      <StoryCommentsHeader 
+        id={id} 
+        title={title} 
+        url={url} 
+        author={author}
+      />
+
+      {/* story comments list */}
+      <StoryCommentsList storyCommentData={children} />
+    </StyledStoryCommentsPanel>
+  );
+}
+
+const StoryCommentsHeader = ({ 
+  id, 
+  title, 
+  url, 
+  author, 
+}) => {
+  return (
+    <StyledStoryCommentsHeader>
+
+    </StyledStoryCommentsHeader>
+  )
+}
+
+const StoryCommentsList = ({ storyCommentListData }) => {
+  if (!storyCommentListData) {
+    return null;
+  }
+
+  return (
+    <StyledStoryCommentsList>
+      {storyCommentListData.map((storyCommentItemData) =>
+        <StoryCommentItem
+          key={storyCommentItemData.id}
+          {...storyCommentItemData}
+        />
+      )}
+    </StyledStoryCommentsList>
+  );
+} 
+
+const StoryCommentItem = ({ 
+  id, 
+  author, 
+  created_at_i: time,
+  text, 
+  children, 
+}) => {   // deconstruct props
+  return (
+    <StyledStoryCommentsItem>
+      <p>{id} | {author} | {time}</p>
+      <p>{text}</p>
+      { children && (
+        <StoryCommentsList storyCommentListData={children} />
+      )}
+    </StyledStoryCommentsItem>
+  );
+}
+//#endregion
+
+export default AppPage;
