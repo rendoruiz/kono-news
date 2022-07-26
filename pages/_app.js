c3413e54b409a4a9175eed45539b33e403a81103
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-07-26 17:19:11 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 // import { ReactQueryDevtools } from 'react-query/devtools'
 
-// import '../styles/reset.css'
 import '../styles/globals.css'
 
 const queryClient = new QueryClient();
