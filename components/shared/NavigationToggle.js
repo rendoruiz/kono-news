599923958edbe964ce0b6631a5e8fee572967a9f
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-07-30 14:37:14 -0600

    update toggle to use context + memo

--- components/shared/NavigationToggle.js
+++ components/shared/NavigationToggle.js
@@ -1,16 +1,24 @@
+import React from "react";
+import { NAVIGATION_ACTION } from "../../utils/constants";
+import { NavigationContext } from "../AppDashboardPage";
 import { FluentLineHorizontal3Regular } from "./FluentIcons";
 
-const NavigationToggle = ({ onClick }) => (
-  <button
-    type='button'
-    title='navigation panel toggle button'
-    className='group border-1 border-transparent rounded ml-1 px-2 py-1 leading-none transition-opacity cursor-pointer hover:opacity-50'
-    onClick={onClick}
-  >
-    <FluentLineHorizontal3Regular 
-      className='w-7 h-7 group-active:scale-[0.8] origin-center transition-transform'
-    />
-  </button>
-);
+const NavigationToggle = React.memo(() => {
+  const dispatch = React.useContext(NavigationContext);
+  const handleClick = () => dispatch({ type: NAVIGATION_ACTION.TOGGLE_PANEL });
+
+  return (
+    <button
+      type='button'
+      title='navigation panel toggle button'
+      className='group border-1 border-transparent rounded ml-1 px-2 py-1 leading-none transition-opacity cursor-pointer hover:opacity-50'
+      onClick={handleClick}
+    >
+      <FluentLineHorizontal3Regular 
+        className='w-7 h-7 group-active:scale-[0.8] origin-center transition-transform'
+      />
+    </button>
+  );
+});
  
 export default NavigationToggle;
