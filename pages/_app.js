65cd18e11396b4c43205ac7568c5f2246942e861
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-08-04 13:18:48 -0600

    add theme provider to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from 'next-themes';
 import { QueryClient, QueryClientProvider } from 'react-query'
 // import { ReactQueryDevtools } from 'react-query/devtools'
 
@@ -6,11 +7,12 @@ import '../styles/globals.css'
 const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }) => (
-  <QueryClientProvider client={queryClient}>
-    <Component {...pageProps} />
-    
-    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-  </QueryClientProvider>
+  <ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  </ThemeProvider>
 )
 
 export default MyApp;
