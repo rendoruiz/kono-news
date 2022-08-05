9831a7ef5646383d1344e4ab56c2fd82f6d654c5
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-08-05 17:24:26 -0600

    add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Open+Sans:wght@300;400;500&display=swap" rel="stylesheet" />
