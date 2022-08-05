946489d45ea4c9b1d6a107b189d1b9226cccf666
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-08-04 23:01:01 -0600

    add elevation

--- components/shared/PillSelectedIndicator.js
+++ components/shared/PillSelectedIndicator.js
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 const PillSelectedIndicator = React.memo(({ isSelected, large = false }) => (
   <div className={clsx(
-    'hidden absolute inset-0 right-auto rounded my-auto w-[3px] h-2/5 bg-KonoAccentLight pointer-events-none',
+    'hidden absolute z-10 inset-0 right-auto rounded my-auto w-[3px] h-2/5 bg-KonoAccentLight pointer-events-none',
     'dark:bg-KonoAccentDark',
     {'md:!block': isSelected},
     {'h-3/5': large}
