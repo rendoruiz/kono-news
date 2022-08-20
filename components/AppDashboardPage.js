6e13bd1877841c27124c1e3978a4352178844d19
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-08-19 19:10:10 -0600

    refactor navigation context
    based on new learnings UwU

--- components/AppDashboardPage.js
+++ components/AppDashboardPage.js
@@ -7,38 +7,12 @@ import NavigationPanel from "./NavigationPanel";
 import StoryListPanel from "./StoryListPanel";
 import StoryDiscussionPanel from "./StoryDiscussionPanel";
 
-import { NavigationContext } from "../context/NavigationContext";
+import { NavigationProvider } from "../context/NavigationContext";
 import { StoryContext } from "../context/StoryContext";
 
-import { NAVIGATION_ACTION, QUERY_KEY, STORYDISCUSSION_ACTION } from "../utils/constants";
-import { parseStoryListModeId } from "../utils/fetchApi";
+import { QUERY_KEY, STORYDISCUSSION_ACTION } from "../utils/constants";
 
 //#region reducer
-const navigationReducer = (state, action) => {
-  switch (action.type) {
-    case NAVIGATION_ACTION.SET_ID: 
-      return {
-        ...state,
-        storyListModeId: (state.storyListModeId !== action.storyListModeId) 
-          ? action.storyListModeId 
-          : state.storyListModeId,
-        isExpanded: false,
-      };
-    case NAVIGATION_ACTION.EXPAND_PANEL:
-      return {
-        ...state,
-        isExpanded: true,
-      }
-    case NAVIGATION_ACTION.RETRACT_PANEL:
-      return {
-        ...state,
-        isExpanded: false,
-      }
-    default:
-      throw new Error();
-  }
-}
-
 const storyDiscussionReducer = (state, action) => {
   switch (action.type) {
     case STORYDISCUSSION_ACTION.SET_ID:
@@ -67,15 +41,7 @@ const storyDiscussionReducer = (state, action) => {
 //#endregion
 
 const AppDashboardPage = ({ initialStoryListModeId, initialStoryDiscussionId, initialIsPermalink, }) => {
-  const [isMounted, setIsMounted] = React.useState(false);
-  const router = useRouter(); 
-  const [navigation, dispatchNavigation] = React.useReducer(
-    navigationReducer, 
-    {
-      isExpanded: false,
-      storyListModeId: null,
-    }
-  );
+  const router = useRouter();
   const [storyDiscussion, dispatchStoryDiscussion] = React.useReducer(
     storyDiscussionReducer,
     {
@@ -84,61 +50,6 @@ const AppDashboardPage = ({ initialStoryListModeId, initialStoryDiscussionId, in
       id: initialStoryDiscussionId,
     }
   );
-  
-  //#region oncomponentmount useeffect
-  // set initial list mode id & remove navigation expansion on mount
-  React.useEffect(() => {
-    if (!isMounted) {
-      setIsMounted(true);
-      
-      const storyListModeId = parseStoryListModeId(initialStoryListModeId);
-      dispatchNavigation({
-        type: NAVIGATION_ACTION.SET_ID,
-        storyListModeId,
-      });
-      // default query string on load, if query string is empty
-      const { 
-        [QUERY_KEY.IS_NAVIGATION_EXPANDED]: _,
-        ...newRouterQuery
-      } = router.query
-      router.replace({
-        query: {
-          ...newRouterQuery,
-          [QUERY_KEY.STORY_LIST_MODE_ID]: storyListModeId,
-        }
-      }, undefined, { shallow: true });
-    }
-  }, [initialStoryListModeId, isMounted, router]);
-  //#endregion
-
-  //#region navigation useeffect
-  // set list mode id based on route query
-  React.useEffect(() => {
-    const { 
-      [QUERY_KEY.STORY_LIST_MODE_ID]: newStoryModeId, 
-    } = router.query;
-
-    if (newStoryModeId && navigation.storyListModeId !== newStoryModeId) {
-      dispatchNavigation({
-        type: NAVIGATION_ACTION.SET_ID,
-        storyListModeId: newStoryModeId,
-      });
-    }
-  }, [router.query, navigation.storyListModeId]);
-
-  // handle navigation panel expansion
-  React.useEffect(() => {
-    const { 
-      [QUERY_KEY.IS_NAVIGATION_EXPANDED]: newIsExpanded,
-    } = router.query;
-
-    if (navigation.isExpanded && !newIsExpanded) {
-      dispatchNavigation({ type: NAVIGATION_ACTION.RETRACT_PANEL });
-    } else if (!navigation.isExpanded && newIsExpanded) {
-      dispatchNavigation({ type: NAVIGATION_ACTION.EXPAND_PANEL });
-    }
-  }, [router.query, navigation.isExpanded]);
-  //#endregion
 
   //#region story discussion useeffect
   // handle story comments panel expansion and set new stroycommentsid
@@ -174,45 +85,8 @@ const AppDashboardPage = ({ initialStoryListModeId, initialStoryDiscussionId, in
   }, [router.query, storyDiscussion.isPermalink]);
   //#endregion
 
-  const handleToggleNavigationPanel = () => {
-    if (!navigation.isExpanded) {
-      router.push(
-        { query: {
-          ...router.query,
-          [QUERY_KEY.IS_NAVIGATION_EXPANDED]: true,
-        }}, 
-        undefined, 
-        { shallow: true }
-      );
-    } else {
-      const {
-        [QUERY_KEY.STORY_LIST_MODE_ID]: newStoryListModeId,
-        [QUERY_KEY.STORY_DISCUSSION_ID]: newStoryDiscussionId,
-        [QUERY_KEY.IS_STORY_DISCUSSION_EXPANDED]: newIsExpanded,
-      } = router.query;
-      // prevent clogging history from navigation toggle button spam clicking
-      if (
-        (newStoryDiscussionId === undefined || (navigation.storyListModeId == newStoryListModeId)) 
-        && (newStoryDiscussionId === undefined || (storyDiscussion.id == newStoryDiscussionId)) 
-        && (newIsExpanded === undefined || (storyDiscussion.isExpanded && newIsExpanded))
-      ) {
-        router.back(2);
-      } else {
-        const {
-          [QUERY_KEY.IS_NAVIGATION_EXPANDED]: _,
-          ...newRouterQuery
-        } = router.query;
-        router.replace(
-          { query: newRouterQuery}, 
-          undefined, 
-          { shallow: true }
-        );
-      }
-    }
-  }
-
   return ( 
-    <NavigationContext.Provider value={handleToggleNavigationPanel}>
+    <NavigationProvider initialStoryListModeId={initialStoryListModeId}>
       <StoryContext.Provider value={storyDiscussion.id}>
         <Head>
           <title>Kono News - A Fluent Hacker News Viewer</title>
@@ -230,13 +104,8 @@ const AppDashboardPage = ({ initialStoryListModeId, initialStoryDiscussionId, in
         )}>
           {!storyDiscussion.isPermalink && (
             <>
-              <NavigationPanel
-                isExpanded={navigation.isExpanded}
-                currentStoryModeId={navigation.storyListModeId}
-              />
-              <StoryListPanel 
-                storyListModeId={navigation.storyListModeId} 
-              />
+              <NavigationPanel />
+              <StoryListPanel />
             </>
           )}
           <StoryDiscussionPanel 
@@ -246,7 +115,7 @@ const AppDashboardPage = ({ initialStoryListModeId, initialStoryDiscussionId, in
           />
         </div>
       </StoryContext.Provider>
-    </NavigationContext.Provider>
+    </NavigationProvider>
   );
 }
 
--- context/NavigationContext.js
+++ context/NavigationContext.js
@@ -1,6 +1,130 @@
-import React from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+import { useRouter } from "next/router";
 
-const NavigationContext = React.createContext(null);
-const useNavigation = () => React.useContext(NavigationContext);
+import { NAVIGATION_ACTION, QUERY_KEY } from '../utils/constants';
+import { parseStoryListModeId } from '../utils/fetchApi';
 
-export { NavigationContext, useNavigation }
+export const NavigationContext = createContext(null);
+
+const navigationReducer = (state, action) => {
+  switch (action.type) {
+    case NAVIGATION_ACTION.SET_ID: 
+      return {
+        ...state,
+        storyListModeId: (state.storyListModeId !== action.storyListModeId) 
+          ? action.storyListModeId 
+          : state.storyListModeId,
+        isExpanded: false,
+      };
+    case NAVIGATION_ACTION.EXPAND_PANEL:
+      return {
+        ...state,
+        isExpanded: true,
+      }
+    case NAVIGATION_ACTION.RETRACT_PANEL:
+      return {
+        ...state,
+        isExpanded: false,
+      }
+    default:
+      throw new Error();
+  }
+}
+
+export const NavigationProvider = ({ children, initialStoryListModeId }) => {
+  const router = useRouter();
+  const [navigation, dispatchNavigation] = useReducer(navigationReducer, {
+
+  });
+
+  // set initial story list mode
+  useEffect(() => {
+    console.log('nav init useeffect()')
+    const storyListModeId = parseStoryListModeId(initialStoryListModeId);
+    dispatchNavigation({
+      type: NAVIGATION_ACTION.SET_ID,
+      storyListModeId,
+    });
+    // default query string on load, if query string is empty
+    const { 
+      [QUERY_KEY.IS_NAVIGATION_EXPANDED]: _,
+      ...newRouterQuery
+    } = router.query
+    router.replace({
+      query: {
+        ...newRouterQuery,
+        [QUERY_KEY.STORY_LIST_MODE_ID]: storyListModeId,
+      }
+    }, undefined, { shallow: true });
+  }, [initialStoryListModeId, router]);
+
+  // navigation panel expansion toggle
+  useEffect(() => {
+    const { 
+      [QUERY_KEY.IS_NAVIGATION_EXPANDED]: newIsExpanded,
+    } = router.query;
+
+    if (navigation.isExpanded && !newIsExpanded) {
+      dispatchNavigation({ type: NAVIGATION_ACTION.RETRACT_PANEL });
+    } else if (!navigation.isExpanded && newIsExpanded) {
+      dispatchNavigation({ type: NAVIGATION_ACTION.EXPAND_PANEL });
+    }
+  }, [router.query, navigation.isExpanded]);
+
+  // set new list mode id
+  useEffect(() => {
+    const { 
+      [QUERY_KEY.STORY_LIST_MODE_ID]: newStoryModeId, 
+    } = router.query;
+
+    if (newStoryModeId && navigation.storyListModeId !== newStoryModeId) {
+      dispatchNavigation({
+        type: NAVIGATION_ACTION.SET_ID,
+        storyListModeId: newStoryModeId,
+      });
+    }
+  }, [router.query, navigation.storyListModeId]);
+
+  const toggleNavigation = () => {
+    if (!navigation.isExpanded) {
+      router.push(
+        { query: {
+          ...router.query,
+          [QUERY_KEY.IS_NAVIGATION_EXPANDED]: true,
+        }}, 
+        undefined, 
+        { shallow: true }
+      );
+    } else {
+      const {
+        [QUERY_KEY.STORY_LIST_MODE_ID]: newStoryListModeId,
+        [QUERY_KEY.STORY_DISCUSSION_ID]: newStoryDiscussionId,
+        [QUERY_KEY.IS_STORY_DISCUSSION_EXPANDED]: newIsExpanded,
+      } = router.query;
+      // prevent clogging history from navigation toggle button spam clicking
+      if (
+        (newStoryDiscussionId === undefined || (navigation.storyListModeId == newStoryListModeId)) 
+        && (newStoryDiscussionId === undefined || (storyDiscussion.id == newStoryDiscussionId)) 
+        && (newIsExpanded === undefined || (storyDiscussion.isExpanded && newIsExpanded))
+      ) {
+        router.back(2);
+      } else {
+        const {
+          [QUERY_KEY.IS_NAVIGATION_EXPANDED]: _,
+          ...newRouterQuery
+        } = router.query;
+        router.replace(
+          { query: newRouterQuery}, 
+          undefined, 
+          { shallow: true }
+        );
+      }
+    }
+  }
+
+  return (
+    <NavigationContext.Provider value={{...navigation, toggleNavigation}}>
+      {children}
+    </NavigationContext.Provider>
+  )
+}
