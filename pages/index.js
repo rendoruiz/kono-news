e552f07e94bdb59801f9f293d61694dd31897bb4
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-09-03 12:15:27 -0600

    remove unnecessary abstraction

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from "next/head";
+import clsx from "clsx";
 
-import AppDashboardPage from "../components/AppDashboardPage";
+import NavigationPanel from "../components/NavigationPanel";
+import StoryDiscussionPanel from "../components/StoryDiscussionPanel";
+import StoryListPanel from "../components/StoryListPanel";
+
+import { NavigationProvider } from "../context/NavigationContext";
+import { StoryDiscussionProvider } from "../context/StoryDiscussionContext";
 
 import { QUERY_KEY } from "../utils/constants";
 
-const HomePage = (props) => (
+const HomePage = ({ initialStoryListModeId, initialStoryDiscussionId, initialIsPermalink }) => (
   <>
     <Head>
       <title>Kono News - A Fluent Hacker News Viewer</title>
@@ -13,7 +19,24 @@ const HomePage = (props) => (
       <meta property="og:description" content="Kono News is a heavily-opinionated Hacker News viewer with a splash of Microsoft's Fluent Design." />
       <meta property="og:url" content="https://news.kono.cx/" />
     </Head>
-    <AppDashboardPage {...props} />
+
+    <NavigationProvider initialStoryListModeId={initialStoryListModeId}>
+      <StoryDiscussionProvider
+        initialIsPermalink={initialIsPermalink}
+        initialStoryDiscussionId={initialStoryDiscussionId}
+      >
+        <div className={clsx(
+          'relative grid mx-auto w-full h-screen max-w-screen-2xl',
+          'md:grid-cols-[1fr_2fr] md:gap-x-1.5',
+          'xl:grid-cols-[1fr_2.5fr]',
+          '2xl:grid-cols-[1fr_3fr] 2xl:gap-x-2 2xl:overflow-hidden 2xl:p-2'
+        )}>
+          <NavigationPanel />
+          <StoryListPanel />
+          <StoryDiscussionPanel />
+        </div>
+      </StoryDiscussionProvider>
+    </NavigationProvider>
   </>
 );
 
